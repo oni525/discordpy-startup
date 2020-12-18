f72a0b568a4bc8175c5f803784b4287250c6f772
oni525 <76197643+oni525@users.noreply.github.com>
2020-12-18 16:01:13 +0900

    add reaction

--- discordbot.py
+++ discordbot.py
@@ -17,9 +17,15 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+    
 @bot.command()
 async def neko(ctx):
     await ctx.send('やらないよ')
+
+    
+@bot.command()
+async def ひん；；(ctx):
+    await ctx.send('よしよしヾ(・ω・｀)')
 
     
 bot.run(token)
